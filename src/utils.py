e2af95f9ad11054373c8d7f94c94b2294e83f44f
Mahes101 <umaselvaraj257@gmail.com>
2024-10-22 19:24:33 +0530

    Hyper_parameter_training

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,11 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from dataclasses import dataclass
 import pickle
 
@@ -21,12 +24,19 @@ def save_object(file_path,obj):
     except Exception as e:
         raise customException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            
             model.fit(X_train, y_train)  # Train model
 
             # Make predictions
